33f08e648deb8e9f27c54bb7d7f5d56c6a6f4c24
AdrianLopez12 <Adrianlopez20000@hotmail.com>
2022-12-01 10:23:24 -0500

    pruebaU2

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'pruebau2-2b2a4',
+    appId: '1:86039515968:web:6d5e43b86278061f0660a5',
+    storageBucket: 'pruebau2-2b2a4.appspot.com',
+    apiKey: 'AIzaSyB1_PEYnz9mkBl-t39t9-Ngm5EuyCtv4kQ',
+    authDomain: 'pruebau2-2b2a4.firebaseapp.com',
+    messagingSenderId: '86039515968',
+  },
+  production: false,
+};
